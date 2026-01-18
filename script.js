0d65bdd6940c183d6ec1cd2f46e787a44bdf012f
conradodeita <132176760+conradodeita@users.noreply.github.com>
2026-01-18 15:50:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const images = [
+  "CAS00742.jpg",
+  "CAS00743.jpg",
+  "CAS00744.jpg"
+];
+
+const bookElement = document.getElementById("book");
+const indicator = document.getElementById("page-indicator");
+
+function createPages() {
+  images.forEach(src => {
+    const page = document.createElement("div");
+    page.className = "page";
+
+    const img = document.createElement("img");
+    img.src = src;
+    img.loading = "lazy"; // melhora performance
+    img.alt = "Foto do álbum";
+
+    page.appendChild(img);
+    bookElement.appendChild(page);
+  });
+}
+
+createPages();
+
+const pageFlip = new St.PageFlip(bookElement, {
+  width: 450,
+  height: 450,
+  size: "stretch",
+  showCover: true,
+  maxShadowOpacity: 0.5,
+  mobileScrollSupport: true,
+  useMouseEvents: true
+});
+
+pageFlip.loadFromHTML(document.querySelectorAll(".page"));
+
+/* Botões */
+document.getElementById("next").onclick = () => pageFlip.flipNext();
+document.getElementById("prev").onclick = () => pageFlip.flipPrev();
+
+/* Indicador */
+pageFlip.on("flip", e => {
+  const page = e.data + 1;
+  const total = pageFlip.getPageCount();
+  indicator.innerText = `${page} / ${total}`;
+});
